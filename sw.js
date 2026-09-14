23b0c68d84de907e12e64d53f1169084a28877ea
SevenGem27 <teo.monf96@gmail.com>
2026-09-14 12:50:54 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,23 @@
-const CACHE_NAME = 'spesa-pwa-v1';
+const CACHE_NAME = 'spesa-pwa-v2';
 const urlsToCache = [
-  './',
   './index.html',
+  './style.css',
+  './app.js',
   './manifest.json'
 ];
 
 self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
