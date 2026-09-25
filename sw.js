5baef364d2d4b37298691d05d4d2a404ca69b88e
SevenGem27 <teo.monf96@gmail.com>
2026-09-25 22:24:36 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'spesa-pwa-v3';
+const CACHE_NAME = 'spesa-pwa-v4';
 const urlsToCache = [
   './index.html',
   './style.css',
@@ -12,11 +12,30 @@ self.addEventListener('install', event => {
       return cache.addAll(urlsToCache);
     })
   );
+  // Forza l'attivazione immediata del nuovo Service Worker
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  // Pulisce le vecchie cache (come la v1 e v2)
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(response => {
+      // Usa la cache se c'è, altrimenti scarica da internet
       return response || fetch(event.request);
     })
   );
